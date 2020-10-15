0db6090f1aecdd62a99ae0f3b4d30e53f1d4b59b
shtwangy <shtwangy@gmail.com>
2020-10-16 01:59:37 +0900

    feat(src/components/Forms/FormDialog.jsx): create form dialog component.
    Issues #5

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Answer} from './Answer';
 export {default as AnswerList} from './AnswerList';
 export {default as Chat} from './Chat';
 export {default as Chats} from './Chats';
+export {default as FormDialog} from './Forms/FormDialog';
