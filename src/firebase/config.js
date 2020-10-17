6ab4ed56da52adc25cd33ec2db1af08de7d7c5d5
shtwangy <shtwangy@gmail.com>
2020-10-17 22:57:30 +0900

    feat(src/firebase): added config for connecting to firebase.
    Closes #6

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBtTWnikqGyE78w0Be65GE48JIWs5lfI2I",
+    authDomain: "react-chatbot-265e3.firebaseapp.com",
+    databaseURL: "https://react-chatbot-265e3.firebaseio.com",
+    projectId: "react-chatbot-265e3",
+    storageBucket: "react-chatbot-265e3.appspot.com",
+    messagingSenderId: "763989037590",
+    appId: "1:763989037590:web:4132d261bdf0761cd01b37",
+    measurementId: "G-MD09ZG2QX4"
+};
