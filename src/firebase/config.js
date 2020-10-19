c936d12d589bb27df2e8b0d68c88793cb3b4daac
shtwangy <shtwangy@gmail.com>
2020-10-20 00:55:28 +0900

    chore(src/firebase/config.js): remove firebase config.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,1 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBtTWnikqGyE78w0Be65GE48JIWs5lfI2I",
-    authDomain: "react-chatbot-265e3.firebaseapp.com",
-    databaseURL: "https://react-chatbot-265e3.firebaseio.com",
-    projectId: "react-chatbot-265e3",
-    storageBucket: "react-chatbot-265e3.appspot.com",
-    messagingSenderId: "763989037590",
-    appId: "1:763989037590:web:4132d261bdf0761cd01b37",
-    measurementId: "G-MD09ZG2QX4"
-};
+export const firebaseConfig = {};
